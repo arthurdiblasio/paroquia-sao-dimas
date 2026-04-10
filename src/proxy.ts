e5b59fd5d359767cce3fbece7cc4574eb6517ea8
arthurdiblasio <arthurdiblasio@gmail.com>
2026-04-10 11:58:44 -0300

    fix: remove debug log for token in proxy function

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,7 +4,6 @@ import jwt from "jsonwebtoken";
 
 export function proxy(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
-  console.log("token", token);
 
   if (!token) {
     return NextResponse.redirect(new URL("/login", req.url));
